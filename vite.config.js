705ca9e43f220a909c81882f41a554e010c3ab82
sachin066 <sachinsaroj066@gmail.com>
2025-02-07 15:55:29 +0530

    Deshboard Demo

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'http://sachin066.github.io/React-Dashboard-Deign-',
   plugins: [react()],
 })
