329839b69edf16744a708e43da05cbaf6c04822f
sachin066 <sachinsaroj066@gmail.com>
2025-02-07 15:09:29 +0530

    Deshboard Demo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 function Sidebar({ setView }) {
     return (
       <div className="w-64 bg-gray-200 dark:bg-gray-800 text-black dark:text-white h-screen p-5 shadow-lg">
-        <h2 className="text-lg font-bold">Rezolv</h2>
+        <h2 className="text-lg font-bold">Dashboard</h2>
         <ul className="mt-4">
           <li 
             className="py-2 px-4 hover:bg-gray-300 dark:hover:bg-gray-700 rounded cursor-pointer"
